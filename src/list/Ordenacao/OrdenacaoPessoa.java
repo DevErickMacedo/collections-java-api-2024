b1a8f709946ce6434eb4c3d5ac0aeff8355b6a88
DevErickMacedo <erickmacedo120@outlook.com>
2024-07-08 23:48:53 -0300

    Resolvendo Desafio OrdenacaoPessoa e Pessoa com Comparator e Comparable
    Signed-off-by: DevErickMacedo <erickmacedo120@outlook.com>

---
+++ src/list/Ordenacao/OrdenacaoPessoa.java
@@ -0,0 +1,41 @@
+package list.Ordenacao;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class OrdenacaoPessoa {
+    private List<Pessoa> pessoaList;
+
+    public OrdenacaoPessoa() {
+        this.pessoaList = new ArrayList<>();
+    }
+
+    public void adicionarPessoa(String nome, int idade, double altura) {
+        pessoaList.add(new Pessoa(nome, idade, altura));
+    }
+
+    public List<Pessoa> ordenarPorIdade(){
+        List<Pessoa> pessoasPorIdade = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorIdade);
+        return pessoasPorIdade;
+    }
+
+    public List<Pessoa> ordenarPorAltura(){
+        List<Pessoa> pessoasPorAltura = new ArrayList<>(pessoaList);
+        Collections.sort(pessoasPorAltura, new ComparatorPorAltura());
+        return pessoasPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoa ordenacaoPessoa = new OrdenacaoPessoa();
+        ordenacaoPessoa.adicionarPessoa("Erick", 19, 1.73);
+        ordenacaoPessoa.adicionarPessoa("João", 30, 1.80);
+        ordenacaoPessoa.adicionarPessoa("Stefanny", 19, 1.60);
+        ordenacaoPessoa.adicionarPessoa("Stelio", 55, 1.90);
+
+        System.out.println(ordenacaoPessoa.ordenarPorIdade());
+        System.out.println(ordenacaoPessoa.ordenarPorAltura());
+    }
+}
