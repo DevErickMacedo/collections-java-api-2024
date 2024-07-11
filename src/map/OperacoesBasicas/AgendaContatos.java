359202b5459fa29656de656f9193d197a21fa2ad
DevErickMacedo <erickmacedo120@outlook.com>
2024-07-11 10:17:30 -0300

    Resolvendo exercicios Map.
    Signed-off-by: DevErickMacedo <erickmacedo120@outlook.com>

--- src/map/OperacoesBasicas/AgendaContatos.java
+++ src/map/OperacoesBasicas/AgendaContatos.java
@@ -1,2 +1,54 @@
-package map.OperacoesBasicas;public class AgendaContatos {
+package map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    //atributo
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        if(!agendaContatoMap.isEmpty()){
+            agendaContatoMap.remove(nome);
+        }
+    }
+
+    public void exibirContato(){
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome){
+        Integer numeroPorNome = null;
+        if(!agendaContatoMap.isEmpty()){
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+        return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Erick", 123456);
+        agendaContatos.adicionarContato("Erick", 5665);
+        agendaContatos.adicionarContato("Erick Macedo", 111111);
+        agendaContatos.adicionarContato("Erick DIO", 654987);
+        agendaContatos.adicionarContato("Stefanny Rocha", 111111);
+        agendaContatos.adicionarContato("Erick", 444444);
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.removerContato("Stefanny Rocha");
+        agendaContatos.exibirContato();
+
+        System.out.println("O Número é: " + agendaContatos.pesquisarPorNome("Erick DIO"));
+    }
+
 }
--- src/map/Ordenacao/AgendaEventos.java
+++ src/map/Ordenacao/AgendaEventos.java
@@ -1,2 +1,53 @@
-package map.Ordenacao;public class AgendaEventos {
+package map.Ordenacao;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.*;
+
+public class AgendaEventos {
+    private Map<LocalDate, Evento> eventosMap;
+
+    public AgendaEventos() {
+        this.eventosMap = new HashMap<>();
+    }
+
+    public void adicionarEvento(LocalDate data, String nome, String atracao){
+        eventosMap.put(data, new Evento(nome, atracao));
+    }
+
+    public void exibirAgenda(){
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        System.out.println(eventosTreeMap);
+    }
+
+    public void obterProximoEvento(){
+        /* Set<LocalDate> dateSet = eventosMap.keySet();
+         * Collection<Evento> values = eventosMap.values();
+         */
+
+        LocalDate dataAtual = LocalDate.now();
+        LocalDate proximaData = null;
+        Evento proximoEvento = null;
+        Map<LocalDate, Evento> eventosTreeMap = new TreeMap<>(eventosMap);
+        for(Map.Entry<LocalDate, Evento> entry : eventosTreeMap.entrySet()){
+            if(entry.getKey().isEqual(dataAtual) || entry.getKey().isAfter(dataAtual)){
+                proximaData = entry.getKey();
+                proximoEvento = entry.getValue();
+                System.out.println("O próximo evento: " + proximoEvento + " acontecerá na data " + proximaData);
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AgendaEventos agendaEventos = new AgendaEventos();
+
+        agendaEventos.adicionarEvento(LocalDate.of(2024, Month.JULY, 11), "Evento 1", "Atracao 1");
+        agendaEventos.adicionarEvento(LocalDate.of(2022, 7, 9), "Evento 2", "Atracao 2");
+        agendaEventos.adicionarEvento(LocalDate.of(2000, Month.JANUARY, 10), "Evento 3", "Atracao 3");
+
+        agendaEventos.exibirAgenda();
+
+        agendaEventos.obterProximoEvento();
+    }
 }
